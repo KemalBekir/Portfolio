a712877c472d89f23eb1ab6b3b8041c3c08312b0
Kemal <outsider.pld@gmail.com>
2023-02-19 09:47:12 +0000

    added Contact, Testimonials, Works

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import Contact from "./components/Contact/Contact";
 import Intro from "./components/Intro/Intro";
 import Menu from "./components/Menu/Menu";
 import Porftolio from "./components/Portfolio/Porftolio";
+import Testimonials from "./components/Testimonials/Testimonials";
 import Topbar from "./components/Topbar/Topbar";
+import Works from "./components/Works/Works";
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="app">
       <Topbar isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Menu isOpen={isOpen} setIsOpen={setIsOpen}/>
+      <Menu isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className="sections">
         <Intro />
         <Porftolio />
+        <Works />
+        <Testimonials />
         <Contact />
       </div>
     </div>
